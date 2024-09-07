808c965415f6b4430645422343ca48b2122867cb
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-09-07 15:35:02 +1200

    feat: add local storage entry when theme button is pressed

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ console.log('Hello World!')
 console.log(`Please feel free to look around my github repos.\n${REPO_URL}`)
 
 // Popper element
-const popper = document.body.querySelector('[data-popper]>h1')
+const popper = document.body.querySelector('[data-popper] .hero__title')
 // Function Popper will take a heading element as an input, then separate
 // each character in it's own span element, and do some magic with it.
 const popperElements = Array.from(popper.children)
@@ -13,6 +13,8 @@ popperElements.forEach((heading) => Popper(heading)) // adds effect to headings
 
 const toggleTheme = () => {
   const current = document.body.dataset.colorTheme
-  document.body.dataset.colorTheme = current === 'light' ? 'dark' : 'light'
+  const newTheme = current === 'light' ? 'dark' : 'light'
+  document.body.dataset.colorTheme = newTheme
+  localStorage.setItem('theme', newTheme)
 }
 window.toggleTheme = toggleTheme
