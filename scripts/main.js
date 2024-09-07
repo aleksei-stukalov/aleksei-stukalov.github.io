4b8fa174585ab708de055258e7cc2548453cb234
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-09-08 01:46:46 +1200

    style: move js popper logic to the html file and update links in navigation menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,10 @@
-import { Popper } from './Popper.js'
-
 const REPO_URL = 'https://github.com/aleksei-stukalov/'
 console.log('Hello World!')
 console.log(`Please feel free to look around my github repos.\n${REPO_URL}`)
 
-// Popper element
-const popper = document.body.querySelector('[data-popper] .hero__title')
-// Function Popper will take a heading element as an input, then separate
-// each character in it's own span element, and do some magic with it.
-const popperElements = Array.from(popper.children)
-popperElements.forEach((heading) => Popper(heading)) // adds effect to headings
-
-const toggleTheme = () => {
+window.toggleTheme = () => {
   const current = document.body.dataset.colorTheme
   const newTheme = current === 'light' ? 'dark' : 'light'
   document.body.dataset.colorTheme = newTheme
   localStorage.setItem('theme', newTheme)
 }
-window.toggleTheme = toggleTheme
